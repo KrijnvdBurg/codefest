313642924acec87441f057d3278c8db4027bf66e
Krijn van der Burg <krijnc@hotmail.com>
2016-03-23 23:41:34 +0100

    Added absence_keys.php, requestPeriodOff.php, updated db_functions ( except requestPeriodOff() ).
    Finished requestPeriodOff (home) fragment. All data is now being send to PHP to be dealt with.

---
+++ android_codefest/app/src/main/java/gsc/codefest/constructors/absence_constr/absence_status.java
@@ -0,0 +1,16 @@
+package gsc.codefest.constructors.absence_constr;
+
+public class absence_status {
+
+    private int id;
+    private String status;
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) { this.status = status; }
+
+}
