2d93096247d012551180a1488acb00e044257533
Pieter Douma <pieter-douma@hotmail.com>
2016-03-23 23:25:07 +0100

    Added menu to Ziek.php

--- codefest_authentication/pages/ziek.php
+++ codefest_authentication/pages/ziek.php
@@ -1,12 +1,16 @@
-<?php $form_path='ziek_files/formoid1/form.php'; require_once $form_path; ?><!DOCTYPE html>
+<?php $form_path='ziek_files/formoid1/form.php'; require_once $form_path;  include('../classes/navbar.class.php'); ?><!DOCTYPE html>
 <html>
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<title>Ziek - Formoid html forms</title>
+	<title>Medewerker - ziekmelden</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="../stylesheets/style.css">
 </head>
-<body class="blurBg-false" style="background-color:#EBEBEB">
-
+<body>
+<?php 
+$NavBar = new NavBar();
+$NavBar->render();
+?>
 {{Formoid}}
 
 </body>
