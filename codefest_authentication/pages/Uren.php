4e3d5355eafc250b0d721d37a7437af1c0473bdf
Pieter Douma <pieter-douma@hotmail.com>
2016-03-23 23:16:58 +0100

    Added menu to Medewerker pages

--- codefest_authentication/pages/Uren.php
+++ codefest_authentication/pages/Uren.php
@@ -1,12 +1,15 @@
-<?php $form_path='uren_files/formoid1/form.php'; require_once $form_path; ?><!DOCTYPE html>
+<?php $form_path='uren_files/formoid1/form.php'; require_once $form_path;  include('../classes/navbar.class.php'); ?><!DOCTYPE html>
 <html>
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<title>Uren - Formoid html forms</title>
+	<title>Medewerker - Uren</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
-<body class="blurBg-false" style="background-color:#EBEBEB">
-
+<body>
+<?php 
+$NavBar = new NavBar();
+$NavBar->render();
+?>
 {{Formoid}}
 
 </body>
