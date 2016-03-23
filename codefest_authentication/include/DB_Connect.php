58bb9d641932c101e848ba24f8702974508ebbb2
Pieter Douma <pieter-douma@hotmail.com>
2016-03-23 13:43:37 +0100

    Added var_dump for host testing

--- codefest_authentication/include/DB_Connect.php
+++ codefest_authentication/include/DB_Connect.php
@@ -10,9 +10,9 @@ class DB_Connect {
     // Connecting to database
     public function connect() {
         require_once '.\cgi_bin\connector.php';
-        
         // Connecting to mysql database
 		$this->conn = new PDO('mysql:host=$host; dbname=$database;', $user , $password);
+		var_dump($this->conn);
         // return database handler
         return $this->conn;
     }
