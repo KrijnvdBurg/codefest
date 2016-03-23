cadb01f76744696ba0a6994e745c1184a3970e8a
Pieter Douma <pieter-douma@hotmail.com>
2016-03-23 12:45:26 +0100

    Added muhammed's DBHandler(PDO)

---
+++ codefest_authentication/include/database_handler.class.php
@@ -0,0 +1,38 @@
+<?php
+require_once ('.\cgi_bin\connector.php');
+    class DatabaseHandler
+    {
+        private $databaseHandle;
+        
+
+        public function __construct($host,$user,$password,$database)
+        {
+			try {
+				//Open connection PDO
+				$dsn='mysql:dbname='.$database.';host='.$host;
+				$this->databaseHandle = new PDO($dsn, $user, $password);
+			} catch (PDOException $e) {
+				echo 'Connection failed: ' . $e->getMessage();
+			}
+        }
+
+        // Execute query
+        public function executeQuery($sql_query)
+        {
+			$result=$this->databaseHandle->query($sql_query);
+			if (is_object($result) && $result->rowCount()>0) {
+				return $result;
+			} else {
+				return false;
+			}	
+		}
+
+        // Close connection
+        public function __destruct()
+        {
+			try {
+				$this->DatabaseHandle = null; //Closes connection
+			} catch (PDOException $e) {
+			}        
+		}
+    }
