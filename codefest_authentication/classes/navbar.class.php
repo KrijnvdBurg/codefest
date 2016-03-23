4f7779b64c4226ff9c47ad8ba9ef1de071f563fd
Pieter Douma <pieter-douma@hotmail.com>
2016-03-23 16:47:00 +0100

    Created start of the navbar. Still requires database to be tested

---
+++ codefest_authentication/classes/navbar.class.php
@@ -0,0 +1,33 @@
+<?php
+class NavBar {
+	private $navbar;
+	//ROLEID moet nog opgehaald worden
+	//menuItems eerste value is path 2de value is item
+
+	function __construct() {
+		$menuItems = array(
+			array ('', ''),
+			array ('', ''),
+			array ('', ''),
+			array ('', '')
+		);
+		$this->navbar = $menuItems;
+	}
+
+	function render() {
+		$menuClasses = array(
+			'1' => 'menuMedewerker',
+			'2' =>  'menuMain',
+			'3' =>  'menuMain'
+		);
+		$menuclass = $menuClasses[$roleID];
+		$result = '<div id="'.$menuclass.'" name="menu"><ul>';
+		
+		foreach($this->navbar as $menuItem){
+			$result .= '<li><a href="'.$menuItem[0].'">'.$menuItem[1].'</a></li>';
+		}
+		$result .= '</ul></div>';
+		return $result;
+	}
+}	
+?>
