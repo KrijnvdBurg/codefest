9fbfc78245994820a7d012b670ab1dcf8f6935ac
Pieter Douma <pieter-douma@hotmail.com>
2016-03-24 00:28:06 +0100

    Added commented code for dynamic roleID and some comments

--- codefest_authentication/classes/navbar.class.php
+++ codefest_authentication/classes/navbar.class.php
@@ -10,17 +10,20 @@ class NavBar {
 	}
 
 	function render() {
+		//MENUID moet nog dynamisch opgehaald worden
 		$stmt = $this->conn->query("SELECT `item`, `path` FROM `menu` WHERE `menu_id` = 1");
 		$resultStmt = $stmt->fetchAll();
 		$menuItems = array();
 
-		//ROLEID moet nog opgehaald worden
+		//ROLEID moet nog dynamisch opgehaald worden
 		$menuClasses = array(
 			'1' => 'menuMedewerker',
 			'2' => 'menuMain',
 			'3' => 'menuMain'
 		);
 		$roleID = 1;
+		/*$t_roleID = $this->conn->query("SELECT `role_id` FROM `users` WHERE `username` = ''.$username.''");
+		$roleID= $t_roleID->fetch();*/
 		$menuclass = $menuClasses[$roleID];
 
 		$result = '<div id="'.$menuclass.'" name="menu"><ul>';
