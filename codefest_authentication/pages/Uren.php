c8a6072ac50bc20433ce9c360a53a7cfd39fa05d
Pieter Douma <pieter-douma@hotmail.com>
2016-03-23 23:20:33 +0100

    Added stylesheet to medewerker pages

--- codefest_authentication/pages/Uren.php
+++ codefest_authentication/pages/Uren.php
@@ -4,6 +4,7 @@
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<title>Medewerker - Uren</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="../stylesheets/style.css">
 </head>
 <body>
 <?php 
--- codefest_authentication/pages/Vakantie.php
+++ codefest_authentication/pages/Vakantie.php
@@ -4,8 +4,9 @@
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<title>Medewerker - Vakantie</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="../stylesheets/style.css">
 </head>
-<body class="blurBg-false" style="background-color:#EBEBEB">
+<body>
 <?php 
 $NavBar = new NavBar();
 $NavBar->render();
