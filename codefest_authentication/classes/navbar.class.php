77e84658ad912bda3f175f9b26f8d2a6aef8807e
Pieter Douma <pieter-douma@hotmail.com>
2016-03-23 22:01:42 +0100

    Edited navbar. Now works for all menu's

--- codefest_authentication/classes/navbar.class.php
+++ codefest_authentication/classes/navbar.class.php
@@ -1,41 +1,36 @@
 <?php
 class NavBar {
 	private $navbar;
-
 	private $conn;
-    function __construct(){
-        require_once '../include/DB_Connect.php';
-        $db         = new Db_Connect();
-        $this->conn = $db->connect();
-    }
 
 	function __construct() {
-		//menuItems eerste value is path 2de value is item
-		$menuItems = array();
-		$stmt = $this->conn->query("SELECT `item`, `path` FROM `menu` WHERE `menu_id` = 1");
-		foreach ($conn->query($stmnt) as $menuQuery) {
-			$temparr = array($menuQuery['path'] => $menuQuery['item']);
-		}
-		$menuItems = array_merge($menuItems, $temparr);
-		$this->navbar = $menuItems;
+		require_once '../include/DB_Connect.php';
+        $db         = new Db_Connect();
+        $this->conn = $db->connect();
 	}
 
 	function render() {
+		$stmt = $this->conn->query("SELECT `item`, `path` FROM `menu` WHERE `menu_id` = 1");
+		$resultStmt = $stmt->fetchAll();
+		$menuItems = array();
+
+		//ROLEID moet nog opgehaald worden
 		$menuClasses = array(
 			'1' => 'menuMedewerker',
 			'2' => 'menuMain',
 			'3' => 'menuMain'
 		);
-
-		//ROLEID moet nog opgehaald worden
-		$roleID = $_POST['roleID'];
+		$roleID = 1;
 		$menuclass = $menuClasses[$roleID];
+
 		$result = '<div id="'.$menuclass.'" name="menu"><ul>';
-		
-		foreach($this->navbar as $menuItem){
-			$result .= '<li><a href="'.$menuItem[0].'">'.$menuItem[1].'</a></li>';
+		foreach ($resultStmt as $row) {
+			$result .= '<li><a href="'.$row['path'].'">'.$row['item'].'</a></li>';
 		}
+
 		$result .= '</ul></div>';
+		var_dump($menuItems);
+		echo $result;
 		return $result;
 	}
 }	
